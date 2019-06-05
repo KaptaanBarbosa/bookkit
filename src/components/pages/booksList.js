64c33cb87bb7df91ac45118fa2958c10508963c6
Siddharth Shrivastava <shri.sid@gmail.com>
2019-06-06 02:00:10 +0530

    adding some UI

--- src/components/pages/booksList.js
+++ src/components/pages/booksList.js
@@ -1,7 +1,11 @@
 import React, {
-    Component
+    Component,
+    Fragment
 }
-from 'react'
+from 'react';
+import {
+    Container, Row, Col, Navbar, NavbarBrand, Nav, NavItem, NavLink, Card, CardBody, CardTitle, CardSubtitle, CardText, Button
+  } from 'reactstrap';  
 
 import {connect} from 'react-redux';
 import {
@@ -15,14 +19,67 @@ class BooksList extends Component {
     render() {
         const books = this.props.books;
         console.log("BOOKS:\n",books,"Props \n",this.props)
-        const ListOfBooks = books ? books.books.map((booksarr)=>{
-            return (<div key={booksarr.id}>
-                <h2> {booksarr.title}</h2>
-                <h2> {booksarr.description}</h2>
-                <h2> {booksarr.price}</h2>
-                </div>)
-        }) : <div> NO book available</div>
-        return (<div> {ListOfBooks} <button onClick={this.props.getbook}></button></div>)
+
+        return (<div> 
+            <Fragment>
+            <Navbar color="faded" light expand="md">
+    
+              <NavbarBrand href="/">
+                React Bootstrap Example
+              </NavbarBrand>
+              <Nav className="ml-auto" navbar>
+    
+                <NavItem className="d-flex align-items-center">
+                  <NavLink className="font-weight-bold" href="/">Home</NavLink>
+                </NavItem>
+                <NavItem className="d-flex align-items-center">
+                  <NavLink className="font-weight-bold" href="https://www.techiediaries.com/react-bootstrap">
+                    Tutorial
+                  </NavLink>
+                </NavItem>
+    
+    
+              </Nav>
+            </Navbar>
+    
+            <Container fluid>
+    
+    
+              <Row>
+                <Col>
+                  {this.props.books.books.map((contact) => (
+    
+                    <Card>
+                      <CardBody>
+                        <CardTitle className="h3 mb-2 pt-2 font-weight-bold">{contact.title}</CardTitle>
+                        <CardSubtitle className="mb-3 font-weight-light text-uppercase">{contact.description}</CardSubtitle>
+                        <CardText className="mb-4">{contact.price}</CardText>
+                        <Row>
+    
+                        <Col>
+                              <Button color="success">Call</Button>
+                        </Col>
+                        <Col sm={'auto'}>
+                              <Button color="warning">Message</Button>
+                        </Col>
+                        <Col sm={'auto'}>
+                              <Button color="success">Email</Button>
+                        </Col>
+                        </Row>
+                      </CardBody>
+                    </Card>
+    
+    
+    
+                  ))}
+                </Col>
+              </Row>
+    
+            </Container>
+          </Fragment>
+            <button onClick={this.props.getbook}></button>
+            
+            </div>)
         }
     }
 
